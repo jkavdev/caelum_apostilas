78d03d9f4961c0f4d51c62e5e4814e6d040785fc
jkavdev <jhonatankolen@gmail.com>
2016-12-01 02:23:56 -0200

    Criando uma classe para cada operação do sistema

---
+++ caelum-fj21/caelum-fj21-agenda/src/br/com/jkavdev/caelum/fj21/servlet/ControllerServlet.java
@@ -0,0 +1,35 @@
+package br.com.jkavdev.caelum.fj21.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.jkavdev.caelum.fj21.mvc.logica.Logica;
+
+@WebServlet("/mvc")
+public class ControllerServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String parametro = req.getParameter("logica");
+		String nomeDaClasse = "br.com.jkavdev.caelum.fj21.mvc.logica." + parametro;
+
+		try {
+			Class<?> classe = Class.forName(nomeDaClasse);
+
+			Logica logica = (Logica) classe.newInstance();
+			String pagina = logica.executa(req, resp);
+
+			req.getRequestDispatcher(pagina).forward(req, resp);
+		} catch (Exception e) {
+			throw new ServletException("Lógica de negócio causou uma exceção: " + e.getMessage(), e);
+		}
+	}
+
+}
